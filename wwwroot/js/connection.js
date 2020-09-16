f4d904da802c6f2fe847d7ef8763cc6362bd9192
sixpeteunder <plenjo15@gmail.com>
2020-09-16 16:17:54 +0300

    Clean up code, UI

---
+++ wwwroot/js/connection.js
@@ -0,0 +1,18 @@
+let handler;
+
+window.Connection = {
+    Initialize: function (interop) {
+        handler = () => interop.invokeMethodAsync("Connection.StatusChanged", navigator.onLine);
+
+        window.addEventListener("online", handler);
+        window.addEventListener("offline", handler);
+
+        handler(navigator.onLine);
+    },
+    Dispose: function () {
+        if (handler !== null) {
+            window.removeEventListener("online", handler);
+            window.removeEventListener("offline", handler);
+        }
+    }
+}
